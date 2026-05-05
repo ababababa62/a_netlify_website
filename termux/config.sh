5b3aec1fa2b9f1e104068d530249c06b66f2395f
个人狗剩 <hello12345678901945@outlook.com>
2026-05-05 03:03:04 +0000

    update termux/config.sh.
    Signed-off-by: 个人狗剩 <hello12345678901945@outlook.com>

--- termux/config.sh
+++ termux/config.sh
@@ -1,4 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg update && pkg upgrade
-pkg install git curl wget vim nano python nodejs clang make termux-api proot -y
+pkg install -y git curl wget vim nano python nodejs clang make termux-api proot
 echo "配置完毕！"
