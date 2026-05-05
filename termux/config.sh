10659439b2830c041adb22aac029804f6011c5a5
个人狗剩 <hello12345678901945@outlook.com>
2026-05-05 02:43:07 +0000

    update termux/config.sh.
    Signed-off-by: 个人狗剩 <hello12345678901945@outlook.com>

--- termux/config.sh
+++ termux/config.sh
@@ -1,8 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "开始配置Termux环境"
-termux-wake-lock
-termux-setup-storage
-termux-change-repo
 pkg update && pkg upgrade
-pkg install git curl wget vim nano python nodejs clang make htop tree jq fzf ripgrep fd exa termux-api proot -y
+pkg install git curl wget vim nano python nodejs clang make termux-api proot -y
 echo "配置完毕！"
